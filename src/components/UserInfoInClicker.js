eabc8708b412457b4e5f760680857d274fee74c2
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-01-19 16:25:42 +0100

    Basic user info component done

--- src/components/UserInfoInClicker.js
+++ src/components/UserInfoInClicker.js
@@ -1,3 +1,36 @@
+import { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+import { API } from '../lib/api';
+import { AUTH } from '../lib/auth';
+
 export default function User() {
-  return <h1>User component</h1>;
+  // const { pk } = useParams();
+  const [userData, setUserData] = useState(null);
+
+  const pk = AUTH.getPayload().sub;
+
+  useEffect(() => {
+    API.GET(API.ENDPOINTS.singleUser(pk))
+      .then(({ data }) => {
+        setUserData(data);
+      })
+      .catch(({ message, response }) => {
+        console.error(message, response);
+      });
+  }, [pk]);
+
+  console.log(userData);
+
+  return (
+    <>
+      <h2>{userData?.username}'s Progress</h2>
+      <p>No. of red packets: {userData?.number_of_red_packets}</p>
+      <p>
+        Items:{' '}
+        {userData?.items.map((item) => (
+          <li>{item.name}</li>
+        ))}
+      </p>
+    </>
+  );
 }
