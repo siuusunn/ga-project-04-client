d66519af4944776cbf1faa9407b67350d8799c34
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-01-19 18:10:18 +0100

    Clicks now accumulates and stored in local storage. Need to fix API request

--- src/components/RedPacketClicker.js
+++ src/components/RedPacketClicker.js
@@ -1,9 +1,70 @@
-import redPacket from '../assets/red-packet1.png';
+import redPacket from '../assets/red-packet.png';
+import '../styles/RedPacketClicker.scss';
+import { useState, useEffect } from 'react';
+import UserInfoInClicker from './UserInfoInClicker';
+import { API } from '../lib/api';
+import { AUTH } from '../lib/auth';
 
 export default function RedPacketClicker() {
+  const [clicks, setClicks] = useState(0);
+  const [userData, setUserData] = useState({
+    email: '',
+    username: '',
+    first_name: '',
+    last_name: '',
+    profile_image: '',
+    number_of_red_packets: 0,
+    items: [],
+    multipliers: 1
+  });
+
+  const pk = AUTH.getPayload().sub;
+
+  let click = 0;
+
+  useEffect(() => {
+    API.GET(API.ENDPOINTS.singleUser(pk)).then(({ data }) => setUserData(data));
+  }, [pk]);
+
+  console.log(userData);
+
+  // const apiReqBody = {
+  //   ...userData,
+  //   number_of_red_packets: click
+  // };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    click += 1;
+    console.log(click);
+    localStorage.setItem('number_of_red_packets', click);
+
+    const apiReqBody = {
+      ...userData,
+      number_of_red_packets: localStorage.getItem('number_of_red_packets')
+    };
+
+    API.PUT(API.ENDPOINTS.singleUser(pk), apiReqBody, API.getHeaders());
+  };
+
+  // setInterval(() => {
+  //   console.log(localStorage.getItem('number_of_red_packets'));
+  //   API.PUT(
+  //     API.ENDPOINTS.singleUser(pk),
+  //     localStorage.getItem('number_of_red_packets'),
+  //     API.getHeaders()
+  //   );
+  // }, 10000);
+
   return (
     <>
-      <img src={redPacket} alt='red-packet-clicker-button'></img>
+      <img
+        src={redPacket}
+        alt='red-packet-clicker-button'
+        onClick={handleClick}
+        className='red-packet-clicker-button'
+      ></img>
+      {/* <UserInfoInClicker /> */}
     </>
   );
 }
