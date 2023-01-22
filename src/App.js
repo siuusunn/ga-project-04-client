4acbe40525f7bfe4f42154565f69dfed66023d1b
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-01-22 21:03:56 +0100

    Unlock button disables based on user items and no. of red packets

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import RedPacketClicker from './components/RedPacketClicker';
-import Items from './components/Items';
 import Register from './components/Register';
 import Login from './components/Login';
 import Leaderboard from './components/Leaderboard';
@@ -18,7 +17,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/clicker' element={<RedPacketClicker />} />
-          <Route path='/items' element={<Items />} />
           <Route path='/users' element={<Leaderboard />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
